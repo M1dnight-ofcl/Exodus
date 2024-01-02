eeb83326c048ef9984e9ea3287bc7b87e9a40590
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-01-02 16:07:50 -0500

    idek

--- _run.bat
+++ _run.bat
@@ -1,12 +1,14 @@
 @echo off
 echo Starting...
-echo Opening VS Code
-code .
-:: start powershell -noexit
-echo Opening Roblox Studio
+echo - Opening VS Code
+start powershell -command "code ."
+echo - Starting PowerShell
+start powershell -noexit
+echo - Opening Roblox Studio
 start "" "C:\Users\M1dnight\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Roblox\Roblox Studio.lnk"
-echo Starting Dev Server...
-rojo serve
+echo - Starting Dev Server...
+start powershell -command "rojo serve"
+start "" http://localhost:34872
 echo Finished!
 timeout /t 3
 goto :eof
